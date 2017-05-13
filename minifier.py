f3a202fc6d0242de51e5ff1e1a34c877ab322e08
vsevolodkulaga <vsevolod.kulaga@reply-corp.com>
2017-05-13 23:25:32 +0300

    heredocs support

--- minifier.py
+++ minifier.py
@@ -12,6 +12,7 @@ class BashFileIterator:
         self.pos = 0
         self.insideString = False
         self.insideComment = False
+        self.insideHereDoc = False
         self._CBE_counter = 0  # Curly Braces Expansion
         self._PBE_counter = 0  # Parenthesis Expansion
         self._stringBeginsWith = ""
@@ -50,6 +51,22 @@ class BashFileIterator:
             i += 1
         return word
 
+    def getPartOfLineAfterPos(self):
+        result = ''
+        i = self.pos + 1
+        while i < len(self.src) and self.src[i] != '\n':
+            result += self.src[i]
+            i += 1
+        return result
+
+    def getPartOfLineBeforePos(self):
+        result = ''
+        i = self.pos - 1
+        while i >= 0 and self.src[i] != '\n':
+            result = self.src[i] + result
+            i -= 1
+        return result
+
     def skipNextCharacters(self, n):
         self.pos += n
 
@@ -58,6 +75,8 @@ class BashFileIterator:
 
     def charactersGenerator(self):
         self._stringBeginsWith = ""
+        hereDocWord = ''
+        _closeHereDocAfterYield = False
         escaped = False
         while self.pos < len(self.src):
             ch = self.src[self.pos]
@@ -66,38 +85,57 @@ class BashFileIterator:
                 escaped = not escaped
             else:
                 # if (ch in "\"'`") and not escaped and not self.insideComment:
-                if (ch in "\"'") and not escaped and not self.insideComment:
+                if (ch in "\"'") and not escaped and not self.isInsideCommentOrHereDoc():
                     if self.insideString and self._stringBeginsWith == ch:
                         self._stringBeginsWith = ""
                         self.insideString = False
                     elif not self.insideString:
                         self._stringBeginsWith = ch
                         self.insideString = True
-                elif ch == "#" and not self.isInsideStringOrCBEOrPBE() and self.getPreviousCharacter() in "\n\t ;":
+                elif ch == "#" and not self.isInsideStringOrHereDocOrCBEOrPBE() and \
+                        self.getPreviousCharacter() in "\n\t ;":
                     self.insideComment = True
-                elif ch == "\n" and self.insideComment:
-                    self.insideComment = False
-                elif ch == '{' and self.getPreviousCharacter() == '$' and not self.insideComment and \
+                elif ch == '{' and self.getPreviousCharacter() == '$' and not self.isInsideCommentOrHereDoc() and \
                         not self.isInsideSingleQuotedString() and not self.isInsideCBE():
                     self._CBE_counter = 1
                 elif ch == '{' and not self.isInsideSingleQuotedString() and self.isInsideCBE():
                     self._CBE_counter += 1
                 elif ch == '}' and not self.isInsideSingleQuotedString() and self.isInsideCBE():
                     self._CBE_counter -= 1
-                elif ch == '(' and self.getPreviousCharacter() == '$' and not self.insideComment and \
+                elif ch == '(' and self.getPreviousCharacter() == '$' and not self.isInsideCommentOrHereDoc() and \
                         not self.isInsideSingleQuotedString() and not self.isInsidePBE():
                     self._PBE_counter = 1
                 elif ch == '(' and not self.isInsideSingleQuotedString() and self.isInsidePBE():
                     self._PBE_counter += 1
                 elif ch == ')' and not self.isInsideSingleQuotedString() and self.isInsidePBE():
                     self._PBE_counter -= 1
+                elif ch == '<' and self.getPreviousCharacter() == '<' and self.getPreviousCharacters(2) != '<<' and \
+                        self.getNextCharacter() != '<' and \
+                        not self.insideComment and not self.isInsideStringOrHereDocOrCBEOrPBE():
+                    # heredoc
+                    self.insideHereDoc = True
+                    hereDocWord = self.getPartOfLineAfterPos()
+                    if hereDocWord[0] == '-':
+                        hereDocWord = hereDocWord[1:]
+                    hereDocWord = hereDocWord.strip().replace('"', '').replace("'", '')
+                elif ch == "\n":
+                    if self.insideComment:
+                        self.insideComment = False
+                    elif self.insideHereDoc and self.getPartOfLineBeforePos() == hereDocWord:
+                        _closeHereDocAfterYield = True
                 escaped = False
             yield ch
+            if _closeHereDocAfterYield:
+                _closeHereDocAfterYield = False
+                self.insideHereDoc = False
             self.pos += 1
         raise StopIteration
 
     def isInsideString(self):
         return self.insideString
+
+    def isInsideHereDoc(self):
+        return self.insideHereDoc
 
     def isInsideDoubleQuotedString(self):
         return self.insideString and self._stringBeginsWith == '"'
@@ -108,14 +146,17 @@ class BashFileIterator:
     def isInsideComment(self):
         return self.insideComment
 
+    def isInsideCommentOrHereDoc(self):
+        return self.insideComment or self.insideHereDoc
+
     def isInsideCBE(self):
         return self._CBE_counter > 0
 
     def isInsidePBE(self):
         return self._PBE_counter > 0
 
-    def isInsideStringOrCBEOrPBE(self):
-        return self.insideString or self.isInsideCBE() or self.isInsidePBE()
+    def isInsideStringOrHereDocOrCBEOrPBE(self):
+        return self.insideString or self.insideHereDoc or self.isInsideCBE() or self.isInsidePBE()
 
 
 def minify(src):
@@ -132,7 +173,7 @@ def minify(src):
     emptyLine = True
     previousSpacePrinted = True
     for ch in it.charactersGenerator():
-        if it.isInsideStringOrCBEOrPBE():
+        if it.isInsideStringOrHereDocOrCBEOrPBE():
             src += ch
         elif ch == "\\" and it.getNextCharacter() == "\n":
             it.skipNextCharacter()
@@ -154,7 +195,7 @@ def minify(src):
     it = BashFileIterator(src)
     src = ""  # result
     for ch in it.charactersGenerator():
-        if it.isInsideStringOrCBEOrPBE() or ch != "\n":
+        if it.isInsideStringOrHereDocOrCBEOrPBE() or ch != "\n":
             src += ch
         else:
             prevWord = it.getPreviousWord()
@@ -171,7 +212,7 @@ def minify(src):
     it = BashFileIterator(src)
     src = ""  # result
     for ch in it.charactersGenerator():
-        if it.isInsideStringOrCBEOrPBE():
+        if it.isInsideStringOrHereDocOrCBEOrPBE():
             src += ch
         elif ch in ' \t' and (it.getPreviousCharacter() == ";" or it.getNextCharacter() == ";"):
             continue
