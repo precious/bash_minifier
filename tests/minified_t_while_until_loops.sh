8246eb3ba1d36a34ee78fde9b632de954b31754d
vsevolodkulaga <vsevolod.kulaga@reply-corp.com>
2017-05-17 17:19:24 +0300

    correct handling for "until" loop

---
+++ tests/minified_t_while_until_loops.sh
@@ -0,0 +1,1 @@
+echo "while";counter=0;while [ "$counter" -lt 10 ];do echo $counter;counter=$((counter+2));done;echo "until";counter=8;until [ "$counter" -lt 0 ];do echo $counter;counter=$((counter-2));done
